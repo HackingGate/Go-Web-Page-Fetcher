f06a46255cf2e8adeb4ec89b42a7fa405091e942
HackingGate <8541644+HackingGate@users.noreply.github.com>
2023-10-16 16:34:01 +0900

    Added fetchURL()

--- main.go
+++ main.go
@@ -4,7 +4,61 @@ import (
 	"fetch/helper"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
 )
+
+func fetchURL(urlString string) {
+	// Parse the URL
+	parsedURL, err := url.Parse(urlString)
+	if err != nil {
+		fmt.Println("Error parsing URL:", err)
+		return
+	}
+
+	resp, err := http.Get(parsedURL.String())
+	if err != nil {
+		fmt.Println("Error fetching the URL:", err)
+		return
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("Error closing response body:", err)
+		}
+	}(resp.Body)
+
+	// Create a directory to store the file
+	dirName := parsedURL.Host + parsedURL.Path + ".html"
+	if err := os.MkdirAll(dirName, 0755); err != nil {
+		fmt.Println("Error creating directory:", err)
+		return
+	}
+
+	filename := path.Join(dirName, "index.html")
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Error closing file:", err)
+		}
+	}(file)
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		fmt.Println("Error saving content to file:", err)
+		return
+	}
+
+	fmt.Println("Content saved to:", filename)
+}
 
 func main() {
 	// Define the metadata flag
@@ -40,5 +94,11 @@ func main() {
 		fmt.Println("num_links:")
 		fmt.Println("images:")
 		fmt.Println("last_fetch:")
+	}
+
+	// Fetch the URLs
+	for _, urlString := range urls {
+		fmt.Println("Fetching", urlString)
+		fetchURL(urlString)
 	}
 }
